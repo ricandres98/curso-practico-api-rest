f4955351515284b6fc3c98aee637018a623449dc
Ricardo <ricardo270298@gmail.com>
2023-05-16 18:34:38 -0400

    - [BUG FIXED] Favicon

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const Dotenv = require('dotenv-webpack');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -39,5 +40,13 @@ module.exports = {
         }),
         new MiniCssExtractPlugin(),
         new Dotenv(),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, "src", "assets"),
+                    to: "assets"
+                }
+            ]
+        })
     ]
 }
